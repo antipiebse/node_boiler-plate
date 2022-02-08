b8597e66c118c3a88e61de541776f573d1ab72d0
antipiebse <s4223425@naver.com>
2022-02-08 15:41:05 +0900

    mongoURI secret set

--- index.js
+++ index.js
@@ -2,10 +2,31 @@ const express =require('express');
 const { trusted } = require('mongoose');
 const app = express();
 const port = 5000;
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://antipiebse:3785@cluster0.b1est.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(()=> console.log('MongoDB Connected...')).catch(err=>console.log(err))
+const bodyParser = require('body-parser');
+const { User }= require('./models/User'); 
+const config = require('./config/key');
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+//application/json
+app.use(bodyParser.json());
 
-app.get('/', (req,res)=> res.send('hello world'));
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI// ,{useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false} why? defolt value mongoose version 6.0 
+).then(()=> console.log('MongoDB Connected...'))
+  .catch(err=>console.log(err))
+
+app.get('/', (req,res)=> res.send(`hello world\n how are you?`));
+
+app.post('/register', (req, res)=>{
+  //회원 가입 시 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 담는 역할
+  const user = new User(req.body);
+  user.save((err, userInfo)=>{
+    if (err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
 
 app.listen(port, () => console.log(`example app listening on port ${port}!`));
 
